aa674679bdc0b63b1553d85ffca698e903e5da6f
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-24 22:03:05 +0300

    24.11.2022

--- Git_Tutorial/Program.cs
+++ Git_Tutorial/Program.cs
@@ -6,26 +6,38 @@
         {
             Console.WriteLine("Hello, World!");
 
-            
-            
+
+
             //  - Versiyon kontrolü --> git --version
             //
             //  - Versiyon güncelleme --> git update-git-for-windows
-            
-            
+            //
+            //  - Config ayarlarını görüntüleme --> git config --list
+            //     - Bu komutu kullandıktan sonra çıkış yapmak için 'Q' tuşuna basabilirsiniz.
+            //
+            //  - Uzak repotan klonlamak için --> git clone https://github.com/oguzkaganer/Git_Tutorial.git
+            //     - Hangi dosya üzerinde komut yazılıyorsa o dosyaya indirir.
+
+
 
 
             //  - Git repo oluşturmak için --> git init
+            //
+            //  - Username ayarı yapmak için --> git config --global user.name "İsim Soyisim"
+            //
+            //  - Usermail ayarı yapmak için --> git config --global user.email "user@gmail.com"
             //
             //  - Bütün değişikleri Stage'e alma işlemi --> git add .
             //
             //  - Stage'den geri alma işlemi --> git reset
             //
-            //  - Stage'dekileri commitleme --> git commit -m "commit mesajı"
+            //  - Stage'dekileri commitleme --> git commit -m "commit mesajı"  OR  git commit -m mesaj (tek kelimelik mesaj olacaksa)
             //
             //  - Commiti silme --> git reset --hard <Bir Önceki Commit ID>  ÖRNEK : git reset --hard 184a7b5e
             //
-            //  - 
+            //  - Commitleri uzak repoya atma --> git push origin master
+            //
+            //  - Uzak repodan değişiklikleri çekme --> git pull
 
 
         }
