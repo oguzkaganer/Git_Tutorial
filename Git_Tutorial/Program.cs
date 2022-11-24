6ca92f85c947626263f66eb01c81fbdb3f969dfd
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-24 18:24:24 +0300

    Add project files.

---
+++ Git_Tutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Git_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+            //
+            // Versiyon kontrolü --> git --version
+            // Versiyon güncelleme --> git update-git-for-windows
+            //
+            // Git repo oluşturmak için --> git init
+
+
+        }
+    }
+}
