e79744210afc3c2bb3366851c8af9a6309ee7c98
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-24 21:37:23 +0300

    mesaj

--- Git_Tutorial/Program.cs
+++ Git_Tutorial/Program.cs
@@ -7,12 +7,25 @@
             Console.WriteLine("Hello, World!");
 
             
+            
+            //  - Versiyon kontrolü --> git --version
             //
-            // Versiyon kontrolü --> git --version
-            // Versiyon güncelleme --> git update-git-for-windows
+            //  - Versiyon güncelleme --> git update-git-for-windows
+            
+            
+
+
+            //  - Git repo oluşturmak için --> git init
             //
-            // Git repo oluşturmak için --> git init
+            //  - Bütün değişikleri Stage'e alma işlemi --> git add .
             //
+            //  - Stage'den geri alma işlemi --> git reset
+            //
+            //  - Stage'dekileri commitleme --> git commit -m "commit mesajı"
+            //
+            //  - Commiti silme --> git reset --hard <Bir Önceki Commit ID>  ÖRNEK : git reset --hard 184a7b5e
+            //
+            //  - 
 
 
         }
